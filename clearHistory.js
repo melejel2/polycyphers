1f5402962f68d26bce3ab3f8bea7375c2872c56e
melejel2 <83751605+melejel2@users.noreply.github.com>
2024-06-19 10:19:46 +0100

    Update clearHistory.js

--- clearHistory.js
+++ clearHistory.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
+    console.log("DOM fully loaded and parsed.");
     var btn = document.getElementById('clear-history-btn');
-    if(btn) {
+    if (btn) {
         console.log("Button found and script running.");
         btn.onclick = function() {
             console.log("Button clicked!");  // Log when button is clicked
