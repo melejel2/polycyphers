e75d207a7503c9b7e09a9758377d0a05af6cacb6
melejel2 <83751605+melejel2@users.noreply.github.com>
2024-06-19 10:08:39 +0100

    Update clearHistory.js

--- clearHistory.js
+++ clearHistory.js
@@ -1,12 +1,12 @@
-window.onload = function() {
-  var btn = document.getElementById('clear-history-btn');
-  if(btn) {
-      btn.onclick = function() {
-          console.log("Button clicked!");
-          window.history.pushState({}, '', window.location.pathname);
-          window.location.reload();
-      };
-  } else {
-      console.log("Clear history button not found.");
-  }
-};
+document.addEventListener('DOMContentLoaded', function() {
+    var btn = document.getElementById('clear-history-btn');
+    if (btn) {
+        btn.onclick = function() {
+            console.log("Button clicked!");
+            window.history.pushState({}, '', window.location.pathname);
+            window.location.reload();
+        };
+    } else {
+        console.log("Clear history button not found.");
+    }
+});
